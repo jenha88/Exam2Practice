2ab6a455d0ea08564f24918f4143fa0860539f44
Jennifer Ha <Jennifer.Ha-1@ou.edu>
2021-10-24 23:50:13 -0500

    finish part5

---
+++ Practice2/PP5/Game.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP5
+{
+    //name,platform,release_date,summary,meta_score,user_review
+
+    public class Game
+    {
+        public string name { get; set; }
+        public string platform { get; set; }
+        public string release_date { get; set; }
+        public string summary { get; set; }
+        public string meta_score { get; set; }
+        public  string user_review { get; set; }
+
+        public Game()
+        {
+            name = "";
+            platform = "";
+            release_date = "";
+            summary = "";
+            meta_score = "";
+            user_review = "";
+        }
+        public Game(string data)
+        {
+            var file = data.Split(',');
+            name = file[0];
+            platform = file[1];
+            release_date = file[2];
+            summary = file[3];
+            meta_score = file[4];
+            user_review = file[5];
+
+
+        }
+        public override string ToString()
+        {
+            return $"{name}";
+        }
+
+    }
+}
--- Practice2/PP5/MainWindow.xaml.cs
+++ Practice2/PP5/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,81 @@ namespace PP5
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<Game> G = new List<Game>();
+        private List<Game> g;
         public MainWindow()
         {
             InitializeComponent();
+
+            var line = File.ReadAllLines("all_games.csv").Skip(1);
+            foreach (var item in line)
+            {
+                G.Add(new Game(item));
+            }
+            PopulateCB(G);
+            PopulateLB(G);
+        }
+        private void PopulateCB(List<Game> gg)
+        {
+            cbBox.Items.Add("All");
+            cbBox.SelectedIndex = 0;
+            foreach (var item in gg)
+            {
+                if (!cbBox.Items.Contains(item.platform))
+                {
+                    cbBox.Items.Add(item.platform);
+                }
+            }
+
+        }
+        private void PopulateLB(List<Game> gMs)
+        {
+            lstbox.Items.Clear();
+            foreach (var item in gMs)
+            {
+                lstbox.Items.Add(item);
+            }
+        }
+
+        List<Game> FilterGame(List<Game> F)
+        {
+            string f = cbBox.SelectedValue.ToString();
+            List<Game> g = new List<Game>();
+            foreach (var item in F)
+            {
+                if (f.ToLower() == "all")
+                {
+                    g.Add(item);
+                }
+                else if (item.platform.Contains(f))
+                {
+                    g.Add(item);
+                }
+            }
+            return g;
+        }
+
+        private void btbExport_Click(object sender, RoutedEventArgs e)
+        {
+            string jj = JsonConvert.SerializeObject(g, Formatting.Indented);
+            File.WriteAllText("games.json", jj);
+
+            MessageBox.Show("You have successfully exported the file! ( •̀ ω •́ )✧");
+        }
+
+        private void lstbox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Game selected = (Game)lstbox.SelectedItem;
+            window w = new window();
+            w.SetupWindow(selected);
+            w.ShowDialog();
+
+        }
+
+        private void cbBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            g = FilterGame(G);
+            PopulateLB(g);
         }
     }
 }

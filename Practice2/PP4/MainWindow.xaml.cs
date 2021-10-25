380acfa1546f00fc3f2d96318cb372b681c0e56f
Jennifer Ha <Jennifer.Ha-1@ou.edu>
2021-10-24 19:52:39 -0500

    started PP5

--- Practice2/PP4/MainWindow.xaml.cs
+++ Practice2/PP4/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,13 +23,32 @@ namespace PP4
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<GOT> G = new List<GOT>();
         public MainWindow()
         {
+            
             InitializeComponent();
-            using (var client= new HttpClient())
-            {
+            
+        }
 
+        private void btb_Click(object sender, RoutedEventArgs e)
+        {
+            using (var client = new HttpClient())
+            {
+                string jsonData = client.GetStringAsync("https://got-quotes.herokuapp.com/quotes").Result;
+                GOT api = JsonConvert.DeserializeObject<GOT>(jsonData);
+                G.Add(api);
+                
+                txtBox.Text= $"{api.quote}--{api.character}";
             }
+        }
+
+        private void btbExport_Click(object sender, RoutedEventArgs e)
+        {
+            string jj = JsonConvert.SerializeObject(G,Formatting.Indented);
+            File.WriteAllText("GOT_Quotes.json", jj);
+
+            MessageBox.Show("YAY! The file has exported successfully!");
         }
     }
 }
